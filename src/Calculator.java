8bc9aae45e81141973b9a54f9200ec2d4f150c4e
TimurJD <tima.peaceworld@gmail.com>
2015-09-09 15:13:34 +0300

    Change user

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,6 +8,5 @@ import com.calculator.view.CalculatorView;
 public class Calculator {
 	public static void main(String[] args) {
 		new CalculatorController(new CalculatorView(), new MathOperator());
-		
 	}
 }

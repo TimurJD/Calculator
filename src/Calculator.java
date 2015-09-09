87addfc08702a79135136de0bf2a51b9433ea006
andrewMistetskii <alexakhmatov@hotmail.com>
2015-09-09 15:11:09 +0300

    Change user

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,5 +8,6 @@ import com.calculator.view.CalculatorView;
 public class Calculator {
 	public static void main(String[] args) {
 		new CalculatorController(new CalculatorView(), new MathOperator());
+		
 	}
 }
